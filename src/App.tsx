d4e395a5e6d5b3dd14ee62221c52683ce9dfb514
Piotr Kucharek <piotrkucharek1@gmail.com>
2022-09-16 18:00:42 +0200

    Newest changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,17 @@
 import React from 'react';
 import './App.css';
 import {
-  AppBar, Box, Button,
-  Container,
+  AppBar,
   createTheme,
   CssBaseline,
-  Paper,
-  Step,
-  StepLabel,
-  Stepper,
   ThemeProvider,
   Toolbar,
   Typography
 } from "@mui/material";
 import Copyright from "./utils/Copyright";
-import {CourseForm} from "./CourseForm";
-import {PersonalDataForm} from "./PersonalDataForm";
-
-const steps = [
-  'Podaj dane osobowe kursanta',
-  'Wybierz kurs'
-]
-
-function getStepContent(step: number) {
-  switch (step) {
-    case 0:
-      return <PersonalDataForm />;
-    case 1:
-      return <CourseForm />;
-    default:
-      throw new Error('Unknown step');
-  }
-}
+import NewCoursePage from "./add-new-course/NewCoursePage";
 
 function App() {
-  const [activeStep, setActiveStep] = React.useState(0);
-
-  const handleNext = () => {
-    setActiveStep(activeStep + 1);
-  };
-
-  const handleBack = () => {
-    setActiveStep(activeStep - 1);
-  };
-
   return (
     <ThemeProvider theme={createTheme()}>
       <CssBaseline />
@@ -62,41 +30,7 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container component="main" maxWidth="sm" sx={{ mb:4 }}>
-        <Paper
-          variant="outlined"
-          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
-          elevation={2}
-        >
-          <Typography component="h1" variant="h4">
-            Dodaj nowego kursanta
-          </Typography>
-          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-            {steps.map((label) => (
-              <Step key={label}>
-                <StepLabel>{label}</StepLabel>
-              </Step>
-            ))}
-          </Stepper>
-          <>
-            {getStepContent(activeStep)}
-            <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-              {activeStep !== 0 && (
-                <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-                  Back
-                </Button>
-              )}
-              <Button
-                variant="contained"
-                onClick={handleNext}
-                sx={{ mt: 3, ml: 1 }}
-              >
-                {activeStep === steps.length - 1 ? 'Dodaj kursanta' : 'Następny krok'}
-              </Button>
-            </Box>
-          </>
-        </Paper>
-      </Container>
+      <NewCoursePage />
       <Copyright />
     </ThemeProvider>
   );
--- src/add-new-course/NewCoursePage.tsx
+++ src/add-new-course/NewCoursePage.tsx
@@ -1,7 +1,94 @@
-import {Container} from "@mui/material";
+import {
+  Button,
+  Container, FormControl, InputLabel, MenuItem,
+  Paper, Select, SelectChangeEvent, Stack,
+  TextField,
+  Typography
+} from "@mui/material";
+import React from "react";
+import axios from "axios";
 
 export default function NewCoursePage() {
-  return <Container>
-    
-  </Container>
+  const [firstName, setFirstName] = React.useState('');
+  const [lastName, setLastName] = React.useState('');
+  const [nationalIdNumber, setNationalIdNumber] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [courseCategory, setCourseCategory] = React.useState('');
+
+  const changeSelectedCategory = (event: SelectChangeEvent) => {
+    setCourseCategory(event.target.value as string);
+  };
+
+  const sendCreateCourse = () => {
+    console.log(firstName, lastName, nationalIdNumber, email, courseCategory);
+    axios.post('http://localhost:8080/api/course', {
+      firstName: firstName,
+      lastName: lastName,
+      nationalIdNumber: nationalIdNumber,
+      email: email,
+      courseCategory: courseCategory,
+    }, {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+    }).then(() => {
+      console.log("request successful")
+    }).catch((error) => {
+      console.log("Something wrong");
+      console.log(error);
+    })
+  }
+
+  return (
+    <Container component="main" maxWidth="sm" sx={{mb: 4}}>
+      <Paper
+        variant="outlined"
+        sx={{my: {xs: 3, md: 6}, p: {xs: 2, md: 3}}}
+      >
+        <Stack>
+          <Typography component="h1" variant="h4">
+            Dodaj nowego kursanta
+          </Typography>
+          <Typography component="h3">
+            Dane osobowe
+          </Typography>
+          <TextField id="outlined-basic" label="Imię" variant="outlined"
+                     margin="normal"
+                     onChange={(event) => setFirstName(event.target.value)}/>
+          <TextField id="outlined-basic" label="Nazwisko" variant="outlined"
+                     margin="normal"
+                     onChange={(event) => setLastName(event.target.value)}/>
+          <TextField id="outlined-basic" label="Pesel" variant="outlined"
+                     margin="normal"
+                     onChange={(event) => setNationalIdNumber(event.target.value)}/>
+          <TextField id="outlined-basic" label="Adres e-mail" variant="outlined"
+                     margin="normal"
+                     onChange={(event) => setEmail(event.target.value)}/>
+          <FormControl sx={{ my:2 }}>
+            <InputLabel id="demo-simple-select-label">Kategoria prawa jazdy</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={courseCategory}
+              label="Kategoria"
+              onChange={changeSelectedCategory}
+            >
+              <MenuItem value="AM">AM</MenuItem>
+              <MenuItem value="A1">A1</MenuItem>
+              <MenuItem value="A2">A2</MenuItem>
+              <MenuItem value="A">A</MenuItem>
+              <MenuItem value="B">B</MenuItem>
+              <MenuItem value="B1">B1</MenuItem>
+              <MenuItem value="C">C</MenuItem>
+              <MenuItem value="D">D</MenuItem>
+            </Select>
+          </FormControl>
+          <Button onClick={() => sendCreateCourse()}>
+            Dodaj
+          </Button>
+        </Stack>
+      </Paper>
+    </Container>
+  );
 }
